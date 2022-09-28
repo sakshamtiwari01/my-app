e4428e5e357f727e520548a1e823ac7d7264104c
Saksham Tiwari <admin@sakshamtiwari.com>
2022-09-28 20:34:14 +0530

    Updates

---
+++ precache-manifest.eb968135dd7efc88db6b4fee47e375f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f75b463e9911f1f5ad5e",
+    "url": "/my-app/static/css/main.64e75279.chunk.css"
+  },
+  {
+    "revision": "f75b463e9911f1f5ad5e",
+    "url": "/my-app/static/js/main.f75b463e.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/my-app/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "cdc2bd35a326ba37abb7",
+    "url": "/my-app/static/js/2.cdc2bd35.chunk.js"
+  },
+  {
+    "revision": "ad00120c43d3e8711497",
+    "url": "/my-app/static/js/runtime~main.ad00120c.js"
+  },
+  {
+    "revision": "edb0a450effcdae4a423f686214a6885",
+    "url": "/my-app/index.html"
+  }
+];
